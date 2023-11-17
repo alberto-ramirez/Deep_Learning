67fceb6c0fb4d0781a8f3c8b64eac909778ac9d1
Alberto Ramirez Bello <alberto_f22@hotmail.com>
2023-11-16 20:11:35 -0600

    Update Capitulo5_RegresionLinealSimple_Polynomial.py

--- Capitulo5_RegresionLinealSimple_Polynomial.py
+++ Capitulo5_RegresionLinealSimple_Polynomial.py
@@ -8,7 +8,6 @@
 ## Date: October 19th, 2023
 ##==============================================================================
 
-from cProfile import label
 import time
 inicio = time.time()
 
@@ -23,7 +22,7 @@ from sklearn.model_selection import train_test_split
 reg = LinearRegression()
 
 ## Obtener las 10 primeras muestras
-df    = pd.read_csv("C:\\Users\\alber\\Documents\\Progra\\Deep_Learning\\aprendizaje_automatico_y_profundo\\fuentes\\casas\\precios_casas.csv")
+df    = pd.read_csv("precios_casas.csv")
 X     = df[ ['sqft_living'] ].values
 y     = df['price'].values
 
@@ -37,26 +36,19 @@ X_ent, X_pru, y_ent, y_pru = train_test_split(X_esc, y_esc, test_size=0.3, rando
 poly = PolynomialFeatures(degree=2)
 X_ent_poly = poly.fit_transform(X_ent)
 X_pru_poly = poly.fit_transform(X_pru)
-#print('Mostrando el contenido del arreglo X_ent_poly:\n', X_ent_poly)
 
 ## Ajustar el model con datos de entrenamiento
 reg.fit(X_ent_poly, y_ent)
 print('Coeficientes: ', reg.coef_)
 print('Interceptcion: ', reg.intercept_)
 print('Precision del modelo: {}'.format(reg.score(X_ent_poly, y_ent)) )
-
 x_fit = np.arange( X_ent.min(), X_ent.max(), 1 )[:, np.newaxis]
 y_p = reg.predict(poly.fit_transform(x_fit) )
 plt.scatter(X_ent, y_ent, label='Puntos de entrenamiento')
 plt.plot(x_fit, y_p)
 plt.legend(loc='upper left')
-
 fin = time.time()
 ejecucion = (fin - inicio)
 print(f'Tiempo total de ejecucion del programa antes de mostrar las graficas: {ejecucion}')
 
 plt.show()
-
-#Coeficientes:  [0.         0.3447626  0.07234282]
-#Interceptcion:  -0.05851055222583371
-#Precision del modelo: 0.1736017131986194
